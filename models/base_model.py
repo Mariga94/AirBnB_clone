062ae296f792d71074e7ca4c565053c9211124c5
Joseph Mariga <jsphmariga@gmail.com>
2022-08-02 22:20:47 +0000

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -7,8 +7,24 @@ import uuid
 class BaseModel:
     """Represents a Base Model"""
 
-    def __init__(self):
-        """Initializes data"""
+    def __init__(self, *args, **kwargs):
+        """Initialize data
+        Args:
+            *args : no values
+            *kwargs (any): new values
+               id argument
+               created_at
+               updated_at
+        """
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "created_at":
+                    self.created_at = value
+                elif key == "updated_at":
+                    self.updated_at = value
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
