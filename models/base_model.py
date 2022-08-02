8227fa985bddf1c7f431128a0e14ca63d0920c87
Joseph Mariga <jsphmariga@gmail.com>
2022-08-02 21:31:08 +0000

    Add Base Model class

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""Defines Base Model"""
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    """Represents a Base Model"""
+
+    def __init__(self):
+        """Initializes data"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Prints string representation"""
+        return "[{}] ({}) <{}>".format(
+                str(self.__class__.__name__), self.id, str(self.__dict__))
+
+    def save(self):
+        """Updates the public instance attribute
+            updated_at with the current datetime
+        """
+        self.updated_at = datetime.now().isoformat()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of
+            __dict__ of the instance.
+        """
+        return vars(self)
