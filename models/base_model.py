6ca4a923777af3825bdb8d5293f2fe942b9ba606
Joseph Mariga <jsphmariga@gmail.com>
2022-08-03 14:38:13 +0000

    Update dict method to return dictionary including a class key and convert time obj to str

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines Base Model"""
 from datetime import datetime
+###from models import storage
 import uuid
 
 
@@ -32,16 +33,21 @@ class BaseModel:
     def __str__(self):
         """Prints string representation"""
         return "[{}] ({}) <{}>".format(
-                str(self.__class__.__name__), self.id, str(self.__dict__))
+                str(self.__class__.__name__), self.id, str(self.__dict__.copy()))
 
     def save(self):
         """Updates the public instance attribute
             updated_at with the current datetime
         """
         self.updated_at = datetime.now().isoformat()
+        #storage
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of
             __dict__ of the instance.
         """
-        return vars(self)
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = str(type(self).__name__)
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
