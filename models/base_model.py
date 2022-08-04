4d7e62abb0c6f24fb9402ebf56b805f7802c8eba
Joseph Mariga <jsphmariga@gmail.com>
2022-08-04 14:52:54 +0000

    Update module

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines Base Model"""
+import models
 from datetime import datetime
-###from models import storage
 import uuid
 
 
@@ -13,9 +13,6 @@ class BaseModel:
         Args:
             *args : no values
             *kwargs (any): new values
-               id argument
-               created_at
-               updated_at
         """
         if kwargs is not None:
             for key, value in kwargs.items():
@@ -28,26 +25,28 @@ class BaseModel:
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Prints string representation"""
         return "[{}] ({}) <{}>".format(
-                str(self.__class__.__name__), self.id, str(self.__dict__.copy()))
+                str(self.__class__.__name__),
+                self.id, str(self.__dict__.copy()))
 
     def save(self):
         """Updates the public instance attribute
             updated_at with the current datetime
         """
-        self.updated_at = datetime.now().isoformat()
-        #storage
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of
             __dict__ of the instance.
         """
         dictionary = self.__dict__.copy()
-        dictionary["__class__"] = str(type(self).__name__)
+        dictionary["__class__"] = str(__class__.__name__)
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
