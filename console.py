b6bd773989b6cc0916d666fe046db48bc4af2cf1
Joseph Mariga <jsphmariga@gmail.com>
2022-08-04 22:21:45 +0000

    Console 0.01

--- console.py
+++ console.py
@@ -5,5 +5,23 @@ console of the command interpreter
 from ast import Pass
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
-    Pass
+    prompt = '(hbnb)'
+
+    def do_EOF(self, arg):
+        """End of file"""
+        return True
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+
+def parse(arg):
+    'Convert a series of zero or more numbers to an argument tuple'
+    return tuple(map(int, arg.split()))
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
