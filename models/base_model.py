b832a60a0214be3b20677f596163b66469e40fb7
mionki <piousmionki@gmail.com>
2022-08-06 20:10:47 +0300

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,52 +1,51 @@
 #!/usr/bin/python3
-"""Defines Base Model"""
 import models
-from datetime import datetime
 import uuid
 
-
+"""
+class BaseModel
+"""
+from uuid import uuid4
+from datetime import datetime
+from models import storage
 class BaseModel:
-    """Represents a Base Model"""
-
+    """A class that acts as a superclass or parent class to all other classes"""
+    
     def __init__(self, *args, **kwargs):
-        """Initialize data
-        Args:
-            *args : no values
-            *kwargs (any): new values
-        """
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                if key == "id":
-                    self.id = value
-                elif key == "created_at":
-                    self.created_at = value
-                elif key == "updated_at":
-                    self.updated_at = value
+        """BaseModel class initialization"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key, value in kwargs.items():
+                """sets values to attributes"""
+                if key != "__class__":
+                    setattr(self,key,value)
+
+            """Convert date and time from string to the preferred format"""
+            if hasattr(self,"created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"],"%Y-%m-%dT%H:%M:%S.%f")
+            elif hasattr(self,"updated_at")and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"],"%Y-%m-%dT%H:%M:%S.%f")
+
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
-        """Prints string representation"""
-        return "[{}] ({}) <{}>".format(
-                str(self.__class__.__name__),
-                self.id, str(self.__dict__.copy()))
+        """String representation of an instance"""
+        return "[{:s}] ({:s}) {}".format(type(self).__name__, self.id,self.__dict__)
 
     def save(self):
-        """Updates the public instance attribute
-            updated_at with the current datetime
-        """
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
-        models.storage.save()
+        """Updates updated_at attribute with current datetime."""
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of
-            __dict__ of the instance.
-        """
-        dictionary = self.__dict__.copy()
-        dictionary["__class__"] = str(__class__.__name__)
-        dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.updated_at.isoformat()
-        return dictionary
+        """returns all instance attribute in a dictionary form"""
+        this_dict = self.__dict__.copy()
+        this_dict["__class__"] = type(self).__name__
+        this_dict["created_at"] = this_dict["created_at"].isoformat()
+        this_dict["updated_at"] = this_dict["updated_at"].isoformat()
+        return this_dict
+
