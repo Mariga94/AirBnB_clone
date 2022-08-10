609e29d344d149e9d6cf924bbc2f37d91bd41775
Joseph Mariga <jsphmariga@gmail.com>
2022-08-10 00:35:08 +0000

    Fix pycodestyling

--- models/__init__.py
+++ models/__init__.py
@@ -6,4 +6,5 @@ Initialize models package
 from models.engine.file_storage import FileStorage
 """A variable storage which is an instance of FileStorage"""
 storage = FileStorage()
+
 storage.reload()
